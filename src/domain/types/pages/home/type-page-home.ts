d3fbe47b4ab71d969ab0175809478e9c2631e62e
BrennoDev <brenno.amaral.marques@gmail.com>
2024-04-22 19:17:24 -0300

    feat: home page

--- src/domain/types/pages/home/type-page-home.ts
+++ src/domain/types/pages/home/type-page-home.ts
@@ -2,4 +2,10 @@ import { IClassDiv } from "../../type-style";
 
 export interface IStylePageHome{
   wrapper: IClassDiv
+  avatar:{
+    header: IClassDiv
+    alignImageAndTexts: IClassDiv
+    image: IClassDiv
+    alignTexts: IClassDiv
+  }
 }
--- src/presentation/pages/style-page-home.ts
+++ src/presentation/pages/style-page-home.ts
@@ -1,7 +1,53 @@
 import { IStylePageHome } from "@/domain/types/pages/home/type-page-home"
 
 const stylePageHome: IStylePageHome = {
-  wrapper: 'bg-transparent h-[200vh]'
+  wrapper: `
+    bg-transparent 
+    h-[200vh]
+  `,
+  avatar:{
+    header:`
+      w-[100vw]
+      sm:w-auto
+      p-5
+
+      bg-gradient-to-b 
+      from-primary/60
+      from-1%
+      
+      sm:from-5%
+      
+      via-background-dark 
+      via-10%
+      
+      sm:via-20%
+      
+      to-background-dark
+      to-75%    
+    `,
+    alignImageAndTexts: `
+      flex
+      flex-wrap
+      justify-center
+      items-center
+      max-w-[1260px]
+      gap-10
+    `,
+    image: `
+      w-48
+      h-48
+      rounded-full
+    `,
+    alignTexts:`
+      w-100%
+      sm:max-w-[100%]
+      md:max-w-[62vw]
+      flex 
+      flex-col
+      items-center
+    `,
+    
+  }
 }
 
 export default stylePageHome
